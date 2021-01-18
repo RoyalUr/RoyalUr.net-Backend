d90067ce065d1f0cc34d9f4b3442b6eb50a60bd2
Sothatsit <padraiglamont@gmail.com>
2021-01-18 13:25:37 +0800

    Update version numbers due to recent changes

--- src/main/java/net/sothatsit/royalurserver/RoyalUr.java
+++ src/main/java/net/sothatsit/royalurserver/RoyalUr.java
@@ -23,7 +23,7 @@ import java.util.logging.Logger;
  */
 public class RoyalUr {
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     public static final Logger logger = Logging.getLogger("main");
 
     private final Config config;
