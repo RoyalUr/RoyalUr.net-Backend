2f2dfc63f379b87e4ea7191464146ab17cf9af37
Padraig Lamont <padraiglamont@gmail.com>
2021-05-14 20:07:47 +0800

    Up the server version with the protocol changes

--- src/main/java/net/sothatsit/royalurserver/RoyalUr.java
+++ src/main/java/net/sothatsit/royalurserver/RoyalUr.java
@@ -24,7 +24,7 @@ import java.util.logging.Logger;
  */
 public class RoyalUr {
 
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.3.0";
     public static final Logger logger = Logging.getLogger("main");
 
     private final Config config;
