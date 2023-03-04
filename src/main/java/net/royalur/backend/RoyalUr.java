8cf84e7c821056773c59e25b28cf938fcd794db3
Padraig Lamont <padraiglamont@gmail.com>
2023-03-04 12:34:41 +0800

    Update version within code

--- src/main/java/net/royalur/backend/RoyalUr.java
+++ src/main/java/net/royalur/backend/RoyalUr.java
@@ -27,7 +27,7 @@ import java.util.logging.Logger;
  */
 public class RoyalUr {
 
-    public static final String VERSION = "2.0.0-SNAPSHOT";
+    public static final String VERSION = "3.0.0-SNAPSHOT";
     public static final Logger logger = Logging.getLogger("main");
 
     private final Config config;
